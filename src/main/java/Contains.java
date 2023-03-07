3843da45673daa00585547f59cf108b70f5a4b59
angieh17c <angieh17c@gmail.com>
2023-03-07 04:18:35 +0000

    Completed Contains

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,14 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+        boolean p = false;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                p = true;
+                break;
+            }
+            p = false;
+        }
+        return p;
     }
 }
